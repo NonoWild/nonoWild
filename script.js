243df8bee11b2278cb7dae2fee18037cfb662dd7
Javiuty <javiiescuadra@gmail.com>
2020-03-06 00:43:52 +0100

    echando la noche

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ fetch("./patronus.json")
     return response.json();
   })
   .then(function(data) {
-    
+    // PISTAS
     for(let i =0; i < data.sparrow.clue.length; i++){
       document.getElementById(i).innerHTML = data.sparrow.clue[i]
     }
+
+    $('.square').click(function(){
+      let id = $(this).attr("id") 
+      let sparrowbn = data.sparrow.bn
+
+      
+      if(sparrowbn[0][0] === 0){
+        console.log("esto va de blanco")
+      }
+
+    })
     
 
 
-
-
-
-
-
   });
-
- 
 
   
