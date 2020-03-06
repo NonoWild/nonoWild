e7f494a2bc4871410ab6355ea2de7ad46b629b4a
Leired7 <leirediez.7@gmail.com>
2020-03-06 02:55:03 +0100

    ajustando comas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let steps = [
+var steps = [
   [0,0,0,0,0],
   [0,0,0,0,0],
   [0,0,0,0,0],
@@ -11,10 +11,10 @@ fetch("./patronus.json")
   .then(function(response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function(data){
     // PISTAS
     for(let i =0; i < data.sparrow.clue.length; i++){
-      document.getElementById(i).innerHTML = data.sparrow.clue[i]
+      document.getElementById(i).innerHTML = data.sparrow.clue[i];
     }
     
     let comprobar = (()=>{
@@ -23,54 +23,54 @@ fetch("./patronus.json")
       for(i=0; i < steps.length; i++){
         for(j=0; j < steps[i].length; j++){
           //AJUSTAMOS EL VALOR DE STEPS
-          let valor = steps[i][j]
+          let valor = steps[i][j];
           if(valor === 2){
-            valor = 0
+            valor = 0;
           }
           //SI ES DISTINTO QUE EL ORIGNAL NO HAS GANADO TODAVÍA
           if(valor !== data.sparrow.bn[i][j]){
-            return false
+            return false;
           }
         }
       }
-      return true
+      return true;
     })
 
 
     $('.square').click(function(){
-      let id = $(this).attr("id") 
-      let sparrowbn = data.sparrow.bn
+      let id = $(this).attr("id");
+      let sparrowbn = data.sparrow.bn;
       
       //PASO 1
       //HAS HECHO CLICK ALGUNA VEZ
       if(steps[id[0]] [id[1]] === 0){
         
-        steps[id[0]] [id[1]] = 1
+        steps[id[0]] [id[1]] = 1;
         
 
       }else if(steps[id[0]] [id[1]] === 1) {
-        steps[id[0]] [id[1]] = 2
+        steps[id[0]] [id[1]] = 2;
       }else{
-        steps[id[0]] [id[1]] = 0
+        steps[id[0]] [id[1]] = 0;
       }
 
 
 
 
       if(steps[id[0]] [id[1]] === 1){
-        $(this).css("background-color", "black")
+        $(this).css("background-color", "black");
       }else if(steps[id[0]] [id[1]] === 2){
-        $(this).css("background-color", "red")
+        $(this).css("background-color", "red");
       }else{
-        $(this).css("background-color", "white")
+        $(this).css("background-color", "white");
       }
 
 
       if(comprobar() === true){
-        alert("¡Enhorabuena has ganado!")
+        alert("¡Enhorabuena has ganado!");
       }
 
-    })
+    });
     
   });
 
